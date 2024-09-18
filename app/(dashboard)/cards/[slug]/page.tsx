b226803bd7763460fd949bcd5e23d8e99cd8e953
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-09-18 22:11:16 +0200

    commit

--- app/(dashboard)/cards/[slug]/page.tsx
+++ app/(dashboard)/cards/[slug]/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+// import React from "react";
+// // Custom components
+// import CardDisplay from "@/components/card-display/card-display";
 
-export default function CardSlug() {
-  return (
-    <div>CardSlug page</div>
-  )
-}
+// export default async function CardSlug({ 
+//   params 
+// }: { 
+//   params: { 
+//     slug: string 
+//   } 
+// }) {
+//   // API call to get card DTO
+
+//   if (!card) {
+//     return <div>Card not found</div>;
+//   }
+
+//   return (
+//     <div className="flex justify-center items-center min-h-screen bg-zinc-900">
+//       <CardDisplay card={card} />
+//     </div>
+//   );
+// }
