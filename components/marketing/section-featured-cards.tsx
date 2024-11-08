3a6241bbcb1aaad07883ca33218085e839f88465
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-11-08 15:09:20 +0100

    commit

--- components/marketing/section-featured-cards.tsx
+++ components/marketing/section-featured-cards.tsx
@@ -1,87 +1,87 @@
-import React from "react";
-// Utils
-import Link from "next/link";
-import clsx from "clsx";
-// Server
-import { getCardsDTO } from "@/app/server/data/cards-dto";
-// Custom components
-import SectionContainer from "@/components/marketing/section-container";
-import CardThumbnail from "@/components/cards-gallery/card-thumbnail";
+// import React from "react";
+// // Utils
+// import Link from "next/link";
+// import clsx from "clsx";
+// // Server
+// import { getCardsDTO } from "@/app/server/data/cards-dto";
+// // Custom components
+// import SectionContainer from "@/components/marketing/section-container";
+// import CardThumbnail from "@/components/cards-gallery/card-thumbnail";
 
-export default async function SectionFeaturedCards() {
-  const cards = await getCardsDTO({
-    limit: 3,
-    filters: {
-      approved: true,
-    },
-    order: { column: "id", direction: "desc" },
-  });
+// export default async function SectionFeaturedCards() {
+//   const cards = await getCardsDTO({
+//     limit: 3,
+//     filters: {
+//       approved: true,
+//     },
+//     order: { column: "id", direction: "desc" },
+//   });
 
-  return (
-    <SectionContainer label="featured-cards" bgImage size="md">
-      <div
-        id="featured-cards-container"
-        className="
-          flex
-          flex-row
-          justify-center
-          lg:justify-between
-          gap-8
-          items-center
-          w-full
-        "
-      >
-        {cards?.map((card, index) => (
-          <div
-            key={card.id}
-            id={`featured-card-${card.id}-container`}
-            className={clsx("flex flex-col justify-start items-center gap-2", {
-              "hidden lg:flex": index === 2,
-              "hidden md:flex": index === 1,
-            })}
-          >
-            <CardThumbnail
-              cardRender={card.initialMode.render}
-              cardName={card.initialMode.name}
-              cardId={card.id}
-              width={"md"}
-            />
-            <div
-              id={`featured-card-${card.id}-info-container`}
-              className="
-                flex
-                flex-col
-                justify-center
-                items-center
-              "
-            >
-              <Link href={`/cards/${card.id}`}>
-                <p className="font-medium hover:text-teal-400 transition-all duration-300">
-                  {card.initialMode.name}
-                </p>
-              </Link>
-              <div
-                id={`featured-card-${card.id}-author-container`}
-                className="
-                  flex
-                  flex-row
-                  justify-center
-                  items-center
-                  gap-1
-                  text-foreground-muted
-                "
-              >
-                <small>by </small>
-                <Link href={`/${card.username}`}>
-                  <small className="hover:text-white hover:underline transition-all duration-300">
-                    {card.username}
-                  </small>
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </SectionContainer>
-  );
-}
+//   return (
+//     <SectionContainer label="featured-cards" bgImage size="md">
+//       <div
+//         id="featured-cards-container"
+//         className="
+//           flex
+//           flex-row
+//           justify-center
+//           lg:justify-between
+//           gap-8
+//           items-center
+//           w-full
+//         "
+//       >
+//         {cards?.map((card, index) => (
+//           <div
+//             key={card.id}
+//             id={`featured-card-${card.id}-container`}
+//             className={clsx("flex flex-col justify-start items-center gap-2", {
+//               "hidden lg:flex": index === 2,
+//               "hidden md:flex": index === 1,
+//             })}
+//           >
+//             <CardThumbnail
+//               cardRender={card.initialMode.render}
+//               cardName={card.initialMode.name}
+//               cardId={card.id}
+//               width={"md"}
+//             />
+//             <div
+//               id={`featured-card-${card.id}-info-container`}
+//               className="
+//                 flex
+//                 flex-col
+//                 justify-center
+//                 items-center
+//               "
+//             >
+//               <Link href={`/cards/${card.id}`}>
+//                 <p className="font-medium hover:text-teal-400 transition-all duration-300">
+//                   {card.initialMode.name}
+//                 </p>
+//               </Link>
+//               <div
+//                 id={`featured-card-${card.id}-author-container`}
+//                 className="
+//                   flex
+//                   flex-row
+//                   justify-center
+//                   items-center
+//                   gap-1
+//                   text-foreground-muted
+//                 "
+//               >
+//                 <small>by </small>
+//                 <Link href={`/${card.username}`}>
+//                   <small className="hover:text-white hover:underline transition-all duration-300">
+//                     {card.username}
+//                   </small>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </SectionContainer>
+//   );
+// }
