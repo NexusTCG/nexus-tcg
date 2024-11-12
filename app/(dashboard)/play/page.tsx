79dfd6e769f86782d44de5416108fd5a859c1d63
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-11-12 14:43:23 +0100

    commit

--- app/(dashboard)/play/page.tsx
+++ app/(dashboard)/play/page.tsx
@@ -31,49 +31,16 @@ export default function Play() {
         gap-8
       "
     >
-      <div
-        id="play-page-content-container"
-        className="
-          flex 
-          flex-col 
-          justify-start 
-          items-center 
-          w-full 
-          border 
-          border-zinc-700 
-          rounded-sm 
-          overflow-hidden
-        "
-      >
-        <div
-          id="play-page-content-header"
-          className="
-            flex
-            flex-col
-            justify-start
-            items-start
-            w-full
-            p-4
-            border-b
-            border-zinc-700
-          "
-        >
-          <h1>Playtest Nexus TCG</h1>
-          <p className="text-sm text-muted-foreground">
-            Book a playtest session with the Nexus TCG's creator.
-          </p>
-        </div>
-        <Cal
-          namespace="playtest-90"
-          calLink="nexus-tcg/playtest-90"
-          style={{
-            width: "100%",
-            height: "100%",
-            overflow: "scroll",
-          }}
-          config={{ layout: "week_view" }}
-        />
-      </div>
+      <Cal
+        namespace="playtest-90"
+        calLink="nexus-tcg/playtest-90"
+        style={{
+          width: "100%",
+          height: "100%",
+          overflow: "scroll",
+        }}
+        config={{ layout: "month_view" }}
+      />
     </div>
   );
 }
