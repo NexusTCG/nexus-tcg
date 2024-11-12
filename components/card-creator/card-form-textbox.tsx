ee89ab104063abc16632dabbb710a9d729109a93
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-11-12 16:01:32 +0100

    only render keyword select on card type === agent

--- components/card-creator/card-form-textbox.tsx
+++ components/card-creator/card-form-textbox.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import React from "react";
-import { useFormContext } from 'react-hook-form';
+import { useFormContext } from "react-hook-form";
 // Utils
-import clsx from "clsx"
+import clsx from "clsx";
 import { calculateBgColor } from "@/app/utils/actions/actions";
 // Custom components
 import KeywordSelect from "@/components/card-creator/keyword-select";
@@ -11,19 +11,19 @@ import CardText from "@/components/card-creator/card-text";
 import LoreText from "@/components/card-creator/lore-text";
 
 type CardFormTextBoxProps = {
-  activeMode: "initial" | "anomaly"
-}
+  activeMode: "initial" | "anomaly";
+};
 
-export default function CardFormTextBox({
-  activeMode
-}: CardFormTextBoxProps) {
+export default function CardFormTextBox({ activeMode }: CardFormTextBoxProps) {
   const { watch } = useFormContext();
-  const cardGrade = watch("nexus_card_data.grade")
-  const energyCost = watch('initialMode.energy_cost');
-  const keywords = watch('initialMode.keywords');
-  const cardText = watch(`${activeMode}Mode.text`)
+  const cardType = watch("initialMode.type");
+  const cardGrade = watch("nexus_card_data.grade");
+  const energyCost = watch("initialMode.energy_cost");
+  const keywords = watch("initialMode.keywords");
+  const cardText = watch(`${activeMode}Mode.text`);
 
-  const bgColorClass50 = activeMode === "anomaly" ? null : calculateBgColor(energyCost, 50)[0];
+  const bgColorClass50 =
+    activeMode === "anomaly" ? null : calculateBgColor(energyCost, 50)[0];
 
   return (
     <div
@@ -33,28 +33,22 @@ export default function CardFormTextBox({
         "flex flex-col justify-start items-start",
         "w-full h-full px-2 pt-1.5 pb-2.5 gap-1.5",
         "border border-b-2 resize-none",
-        bgColorClass50 || "bg-neutral-50",
+        bgColorClass50 || "bg-neutral-50"
       )}
     >
-      {activeMode === "initial" && (
+      {activeMode === "initial" && cardGrade && cardType === "agent" && (
         <div className="flex-shrink-0">
           <KeywordSelect
-            cardGrade={
-              cardGrade 
-                ? cardGrade.toLowerCase() 
-                : "core"
-              } 
+            cardGrade={cardGrade ? cardGrade.toLowerCase() : "core"}
             truncateKeywords={
-              (cardText > 150 ? true : false) || 
+              (cardText > 150 ? true : false) ||
               (keywords.length > 2 ? true : false)
-            } 
+            }
           />
         </div>
       )}
       <div className="flex-grow w-full overflow-hidden">
-        <CardText 
-          activeMode={activeMode}
-        />
+        <CardText activeMode={activeMode} />
       </div>
       {cardText.length < 150 && keywords.length <= 2 && (
         <div className="flex-shrink-0 w-full">
