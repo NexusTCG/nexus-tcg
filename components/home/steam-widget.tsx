2b3179f5b19834cc9a35f44c4befb0e50069db2b
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-12-03 18:25:03 +0100

    commit

--- components/home/steam-widget.tsx
+++ components/home/steam-widget.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+// Utils
+import Image from "next/image";
 // Components
 import {
   Card,
@@ -7,6 +9,13 @@ import {
   CardTitle,
   CardDescription,
 } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { MdOpenInNew } from "react-icons/md";
 
 export default function SteamWidget() {
   return (
@@ -23,11 +32,43 @@ export default function SteamWidget() {
           px-4
         "
       >
-        <CardTitle className="text-lg">Steam Workshop</CardTitle>
-        <CardDescription>
-          Subscribe to the Nexus TCG Tabletop Simulator mod on Steam Workshop to
-          play.
-        </CardDescription>
+        <CardTitle className="text-lg">Play Nexus TCG</CardTitle>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger
+              className="
+                flex
+                flex-row
+                justify-center
+                items-center
+                gap-1
+                text-muted-foreground
+                hover:text-foreground
+                transition-colors
+                duration-300
+                cursor-pointer
+              "
+            >
+              <a
+                href="https://steamcommunity.com/sharedfiles/filedetails/?id=3279618215"
+                rel="noreferrer"
+                target="_blank"
+                className="
+                  text-sm
+                  text-muted-foreground
+                  hover:text-foreground
+                  transition-colors
+                  duration-300
+                  inline
+                "
+              >
+                Steam Workshop
+              </a>
+              <MdOpenInNew className="w-[1rem] h-[1rem]" />
+            </TooltipTrigger>
+            <TooltipContent side="bottom">Go to Steam Workshop</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </CardHeader>
       <CardContent
         className="
@@ -37,17 +78,65 @@ export default function SteamWidget() {
           items-center
           w-full
           overflow-hidden
-          px-4
           rounded-b-md
+          p-0
         "
       >
-        <iframe
-          src="https://steamcommunity.com/workshop/filedetails/?id=3279618215&embed=true"
-          width="100%"
-          height="500"
-          style={{ border: 0, overflow: "hidden" }}
-          allowFullScreen
-        ></iframe>
+        <div
+          id="steam-workshop-banner"
+          style={{ position: "relative" }}
+          className="
+            w-full 
+            h-[300px] 
+            group
+          "
+        >
+          <Image
+            src="/images/steam-workshop-banner.jpg"
+            alt="Nexus TCG Steam Workshop"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="
+            absolute 
+            inset-0 
+            bg-black/70 
+            opacity-0 
+            group-hover:opacity-100 
+            transition-opacity 
+            duration-300 
+            flex 
+            flex-col
+            justify-end
+            items-start 
+            text-white
+            gap-2
+            p-4
+          "
+          >
+            <p className="font-semibold">Nexus Trading Card Game Mod</p>
+            <p className="text-sm max-w-[80%]">
+              Nexus TCG is available as a free Steam Workshop mod to Tabletop
+              Simulator.{" "}
+              <a
+                href="https://steamcommunity.com/sharedfiles/filedetails/?id=3279618215"
+                rel="noreferrer"
+                target="_blank"
+                className="
+                text-teal-400 
+                hover:text-teal-500 
+                transition-colors 
+                duration-300 
+                hover:underline
+                inline
+              "
+              >
+                Subscribe to play!
+              </a>
+            </p>
+          </div>
+        </div>
       </CardContent>
     </Card>
   );
