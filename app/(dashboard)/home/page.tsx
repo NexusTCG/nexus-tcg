aa1c0dc61ef66204ee7ffa4129914b19b5d57b7e
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-12-08 17:52:52 +0100

    commit

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,20 +1,11 @@
 import React, { Suspense } from "react";
 // Utils
 import dynamic from "next/dynamic";
-// Data
-// import { getCardsDTO } from "@/app/server/data/cards-dto";
-// Types
-// import { CardDTO } from "@/app/lib/types/dto";
-// import ReadyToPlay from "@/components/home/ready-to-play";
 // Components
 import { Skeleton } from "@/components/ui/skeleton";
 // Dynamic custom components
 const SteamWidget = dynamic(() => import("@/components/home/steam-widget"));
 const LatestCards = dynamic(() => import("@/components/home/latest-cards"));
-const LatestNotifications = dynamic(
-  () => import("@/components/home/latest-notifications")
-);
-const TopCards = dynamic(() => import("@/components/home/top-cards"));
 const QuickLearn = dynamic(() => import("@/components/home/quick-learn"));
 const WeeklyTrends = dynamic(() => import("@/components/home/weekly-trends"));
 const PlaceholderCard = dynamic(
@@ -22,30 +13,6 @@ const PlaceholderCard = dynamic(
 );
 
 export default async function Home() {
-  // const currentWeekCards = await getCardsDTO({
-  //   order: { column: "created_at", direction: "desc" },
-  //   currentWeekOnly: true,
-  // });
-
-  // Count the number of cards for each type
-  // const cardTypeCounts =
-  //   currentWeekCards?.reduce((acc, card: CardDTO) => {
-  //     const type = card.initialMode.type;
-  //     acc[type] = (acc[type] || 0) + 1;
-  //     return acc;
-  //   }, {} as Record<string, number>) || {};
-
-  // Filter out types with no cards
-  // const cardTypes = Object.entries(cardTypeCounts)
-  //   .filter(([_, count]) => count > 0)
-  //   .map(([type]) => type);
-
-  // Filter out the top 10 cards by votes
-  // const topCards =
-  //   currentWeekCards
-  //     ?.sort((a, b) => (b.votes || 0) - (a.votes || 0))
-  //     .slice(0, 10) || [];
-
   return (
     <div
       id="home-page-container"
@@ -80,14 +47,6 @@ export default async function Home() {
           <Suspense fallback={<Skeleton className="w-full h-[300px]" />}>
             <LatestCards />
           </Suspense>
-          {/* <Suspense fallback={<Skeleton className="w-full h-[300px]" />}>
-            {topCards.length > 0 ? (
-              <TopCards topCards={topCards} />
-            ) : (
-              <PlaceholderCard card="top-cards" />
-            )}
-          </Suspense> */}
-          {/* <LatestNotifications /> */}
           <SteamWidget />
         </div>
         <div
@@ -100,8 +59,6 @@ export default async function Home() {
             gap-4
           "
         >
-          {/* TODO: Implement matchmaking system with Cal.com */}
-          {/* <ReadyToPlay /> */}
           <QuickLearn />
           <Suspense fallback={<PlaceholderCard card="weekly-trends" />}>
             <WeeklyTrends />
