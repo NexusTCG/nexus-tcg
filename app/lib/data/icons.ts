242957c4ab73b65c55099eba6b3fd382d50a6ca8
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-12-27 23:14:23 +0100

    commit

--- app/lib/data/icons.ts
+++ app/lib/data/icons.ts
@@ -27,8 +27,8 @@ import speed2 from "@/public/icons/speed-icons/speed-2.svg";
 import speed3 from "@/public/icons/speed-icons/speed-3.svg";
 // Grade icons
 import core from "@/public/icons/grade-icons/core.svg";
-import uncommon from "@/public/icons/grade-icons/uncommon.svg";
 import rare from "@/public/icons/grade-icons/rare.svg";
+import epic from "@/public/icons/grade-icons/epic.svg";
 import prime from "@/public/icons/grade-icons/prime.svg";
 
 export const energyIcons = {
@@ -62,8 +62,8 @@ export const speedIcons = [speed1, speed2, speed3];
 
 export const gradeIcons = [
   { name: "core", icon: core },
-  { name: "uncommon", icon: uncommon },
   { name: "rare", icon: rare },
+  { name: "epic", icon: epic },
   { name: "prime", icon: prime },
 ];
 
@@ -98,8 +98,8 @@ export const abbreviationIcons = {
   "S3": { path: "/icons/speed-icons/speed-3.svg", name: "Speed: 3" },
   // Grade icons
   "GC": { path: "/icons/grade-icons/core.svg", name: "Grade: Core" },
-  "GU": { path: "/icons/grade-icons/uncommon.svg", name: "Grade: Uncommon" },
   "GR": { path: "/icons/grade-icons/rare.svg", name: "Grade: Rare" },
+  "GE": { path: "/icons/grade-icons/epic.svg", name: "Grade: Epic" },
   "GP": { path: "/icons/grade-icons/prime.svg", name: "Grade: Prime" },
   // Reach icons
   "RM": { path: "/icons/reach-icons/melee.svg", name: "Reach: Melee" },
